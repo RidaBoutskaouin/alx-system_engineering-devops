d5b181514fc5210db9e07cc00e0cf59399755ff6
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-29 18:08:13 +0100

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """get data from API"""
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = argv[1]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todo = requests.get(url + "todos?userId={}".format(user_id)).json()
+    completed = []
+    for task in todo:
+        if task.get("completed") is True:
+            completed.append(task.get("title"))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), len(completed), len(todo)
+        )
+    )
+    for task in completed:
+        print("\t {}".format(task))
