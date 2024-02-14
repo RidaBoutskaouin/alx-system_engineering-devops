8dcff0b8ee9ff3ae56c3d70f7fe76d6b4bae3f19
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-02-14 20:40:59 +0100

    pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,13 @@ import requests
 def top_ten(subreddit):
     """Function to get the number of subscribers of a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
     else:
         for i in range(10):
-            print(response.json().get('data').get('children')[i].get('data').get('title'))
+            print(
+                response.json().get("data")
+                .get("children")[i].get("data").get("title")
+            )
